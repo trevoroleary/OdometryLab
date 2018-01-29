322b7fe73688bf906419ea587330944f1d335e74
trevorolearyPC <trevor.oleary@mail.mcgill.ca>
2018-01-29 13:50:55 -0500

    Final Version
    Used in Demo

--- src/ca/mcgill/ecse211/odometer/OdometryCorrection.java
+++ src/ca/mcgill/ecse211/odometer/OdometryCorrection.java
@@ -22,6 +22,7 @@ public class OdometryCorrection implements Runnable {
   private SampleProvider sensorSample;
   private float[] sensorData;
   private double sensorOffset = 4.5;
+  private double TILE_SIZE = 30.48;
   
   private boolean firstLineY = true;
   private boolean firstLineX = true;
@@ -53,30 +54,39 @@ public class OdometryCorrection implements Runnable {
     while (true) {
       correctionStart = System.currentTimeMillis();
       
-      // TODO Trigger correction (When do I have information to correct?)
       /**
-       * This updates color sensor data into a float variable
-       * as well as the most recent location data
+       * coordinates are all based on where the center of the wheel base is located
+       * 
+       */
+      
+      /*
+       * Following variables used to store the sensor data
+       * sensor returns data as SampleProvider
+       * Object used to return data in an array 'dataSample
+       * location of the odometer is also stored
        */
       Sensor.fetchSample(sensorData, 0);
       float dataSample = sensorData[0];
-      //boolean correctLocation = false;
       double[] location = new double[3];
-      location = odometer.getXYT();
+      location = odometer.getXYT();     
       
-      
-      
-      
-      /**
-       * The following conditions will only occur if the location should be corrected
-       * The conditions are split up by vertical and horizontal orientations
+      /*
+       * These double variables are used to determine the distance from a black line
+       * later on if these variables are too far from an integer when a black line is seen
+       * the robot decides that it was not actually a line
+       * 15.24 is half the length of a square
        */
-      
-      
-      double yLine = (location[1] + sensorOffset) / 15.24;
-      double xLine = (location[0] + sensorOffset ) / 15.24;
+      double yLine = (location[1] + sensorOffset) / TILE_SIZE;
+      double xLine = (location[0] + sensorOffset ) / TILE_SIZE;
       double angle = location[2] % 360;
       
+      
+      /*
+       * The sensor is placed ahead of the wheel base
+       * if the sensor detects a line the wheel base is 4.5cm behind
+       * these conditions decide weather the offset should be positive or negative
+       * depending on the direction the robot if facing.
+       */
       if((angle < 20 || angle > 340) || (angle < 290 && angle > 250)) {
     	  sensorOffset = 4.5;
       }
@@ -87,46 +97,36 @@ public class OdometryCorrection implements Runnable {
       //Correction for North & South orientations
       
       if(dataSample > 10 && ( (angle < 20 || angle > 340) || ((angle < 200 && angle > 160 )))) {
-    	  
     	  if(firstLineY) {
     		  odometer.setY(-sensorOffset);
     		  firstLineY = false;
     		  Sound.beep();
     	  }
-    	  
-    	  else if(yLine > 1.5 && yLine < 2.5) {
-    		  odometer.setY(30.48-sensorOffset);
+    	  else if(yLine > 0.7 && yLine < 1.3) {
+    		  odometer.setY(TILE_SIZE-sensorOffset);
     		  Sound.beep();
     	  }
-    	  
-    	  else if( yLine > 3.5 && yLine < 4.5) {
-    		  odometer.setY(60.96-sensorOffset);
+    	  else if( yLine > 1.7 && yLine < 2.3) {
+    		  odometer.setY((TILE_SIZE*2)-sensorOffset);
     		  Sound.beep();
     	  }
       }
       
       //Correction for East & West orientations
       if(dataSample > 10 && ((angle < 290 && angle > 250) || (angle < 110 && angle > 70)))
-    	  
     	  if(firstLineX) {
     		  odometer.setX(-sensorOffset);
     		  firstLineX = false;
     		  Sound.beep();
     	  }
-    	  
-    	  else if(xLine > 1.5 && xLine < 2.5) {
-    		  odometer.setX(30.48 - sensorOffset);
+    	  else if(xLine > 0.7 && xLine < 1.3) {
+    		  odometer.setX(TILE_SIZE - sensorOffset);
     		  Sound.beep();
     	  }
-    	  
-    	  else if(xLine > 3.5 && xLine < 4.5) {
-    		  odometer.setX(60.96 - sensorOffset);
+    	  else if(xLine > 1.7 && xLine < 2.3) {
+    		  odometer.setX((TILE_SIZE*2) - sensorOffset);
     		  Sound.beep();
     	  }
-      
-      // TODO Update odometer with new calculated (and more accurate) vales
-
-      //odometer.setXYT(0.3, 19.23, 5.0);
 
       // this ensure the odometry correction occurs only once every period
       correctionEnd = System.currentTimeMillis();
